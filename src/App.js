bdb1196dcd00276f3803b5d7e14c837f1bf8eb1f
fdavison1 <fred.noreply@gmail.com>
2020-01-10 17:34:33 -0700

    login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import React from 'react';
 import Calculator from './Calculator'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+
+class App extends React.Component {
+  state = {}
+  render(){
+    const {authenticated} = this.state;
+    let body = null;
+    if (authenticated) {
+      body = (
+        <div className="page-body">
+          <div className="login-buttons">
+            <button onClick={this.logout}>Logout</button>
+          </div>
+          <Calculator></Calculator>
+        </div>
+      );
+    } else {
+      body = (
+        <div className="page-body">
+          <div className="login-buttons">
+            <button onClick={this.login}>Login</button>
+          </div>
+        </div>
+      );
+    }
+  
+    return (
+      <div className="App">
+        <h1>Calculator</h1>
+        {body}
+      </div>
+    );
+  }
 }
 
 export default App;
